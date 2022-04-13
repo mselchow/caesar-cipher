761aef279de704a60c80848da0bf105025c8d9b5
Michael Selchow <mselchow@pm.me>
2022-04-13 15:26:40 -0400

    Add test cases

--- substrings/spec/substring_spec.rb
+++ substrings/spec/substring_spec.rb
@@ -1,7 +1,23 @@
 require "spec_helper.rb"
 
 RSpec.describe do
+  it "should return a hash" do
+    expect(substrings("hello", ["ll"])).to be_a(Hash)
+  end
 
+  it "should return an empty hash if no matches" do
+    expect(substrings("hello", ["zz"])).to eq({})
+  end
 
+  it "should work for a single word" do
+    expect(substrings("hello", ["ll"])).to eq({"ll" => 1})
+  end
 
+  it "should work for multiple words" do
+    expect(substrings("hello world, holla girl", ["ll"])).to eq({"ll" => 2})
+  end
+
+  it "should work for multiple words with multiple matches" do
+    expect(substrings("hello world, holla girl", ["ll", "or"])).to eq({"ll" => 2, "or" => 1})
+  end
 end
